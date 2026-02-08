69a1a75eb7a324c9e5b4e54e8c273a193795c715
Adnan Karšić <adokce@gmail.com>
2026-02-08 01:43:24 +0100

    Expand three type shim

--- types/three.d.ts
+++ types/three.d.ts
@@ -1,1 +1,25 @@
-declare module "three";
+declare module "three" {
+  export class Vector3 {
+    constructor(x?: number, y?: number, z?: number);
+    [key: string]: any;
+  }
+
+  export class Euler {
+    constructor(x?: number, y?: number, z?: number, order?: string);
+    [key: string]: any;
+  }
+
+  export class Matrix4 {
+    constructor();
+    [key: string]: any;
+  }
+
+  export class Mesh {
+    constructor(...args: any[]);
+    [key: string]: any;
+  }
+
+  export const MathUtils: {
+    euclideanModulo: (value: number, mod: number) => number;
+  };
+}
